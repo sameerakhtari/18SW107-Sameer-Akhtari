14722906400bea7e63d68c9f696e59a5580aa5ec
Sameer Akhtari <sameer_akhtari@hotmail.com>
2019-05-12 14:33:08 +0500

    Add files via upload

---
+++ Lab5/Task3/Detail.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Detail{
+	public static void main(String []abcd)
+	{
+		Scanner s=new Scanner(System.in);
+		String name,breed,age;
+		System.out.print("Enter Dog Name: ");
+		name=s.nextLine();
+		System.out.print("Enter Dog Breed: ");
+		breed=s.nextLine();
+		System.out.print("Enter Dog Age: ");
+		age=s.nextLine();
+		Dog o=new Dog(name,breed,age);
+		o.showDetails();
+	}
+}
---
+++ Lab5/Task3/Dog.java
@@ -0,0 +1,22 @@
+class Dog{
+	String name;
+	String breed;
+	String age;
+	Dog(String name, String breed, String age){
+		this.name=name;
+		this.breed=breed;
+		this.age=age;
+		}
+	public void showDetails(){
+		if(name.isEmpty() && breed.isEmpty() && age.isEmpty()){
+			System.out.print("details are unavailable");
+			}
+		else
+		{
+			System.out.println("Name:"+name);
+			System.out.println("Breed:"+breed);
+			System.out.println("Age:"+age);
+		}
+	}
+}
+		
