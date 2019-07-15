e053ed1c4ba67923c7505fa88073f2cfb38426f9
Sameer Akhtari <sameer_akhtari@hotmail.com>
2019-07-16 01:00:42 +0500

    Add files via upload

---
+++ Lab8/Task2/Main.java
@@ -0,0 +1,10 @@
+class Main
+{
+	public static void main(String []abcd)
+	{
+		Soccer s =new Soccer();
+		Cricket c=new Cricket();
+		s.getNumberOfTeamMembers();
+		c.getNumberOfTeamMembers();
+	}
+}
